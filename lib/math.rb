6e5675128ce85bb16af455db6b637e146bfcce9d
Rohit Pratti <rohit.pratti@gmail.com>
2019-05-15 02:23:57 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,21 +1,54 @@
 def addition(num1, num2)
+  num3 = num1 + num2
+  
+  return num3
+  
+  
 end
 
 def subtraction(num1, num2)
+  
+  num3 = num1 - num2
+  
+  return num3
+  
 end
 
 def division(num1, num2)
+  
+  num3 = num1/num2
+  
+  return num3
+  
 end
 
 def multiplication(num1, num2)
+
+num3 = num1*num2
+
+return num3
+
 end
 
 def modulo(num1, num2)
+
+num3 = num1%num2
+
+return num3
+
 end
 
 def square_root(num)
+
+num2 = Integer.sqrt(num)
+
+return num2
+
 end
 
 def order_of_operation(num1, num2, num3, num4)
-  #Hint:  __ + (( __ * __ ) / __ )
+  total = num1 + (( num2 * num3 ) / num4 )
+  
+  return total
+
 end
