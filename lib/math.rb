e77aa586d495ac6936fafa540cf62ff6a0e807c9
Rohit Pratti <rohit.pratti@gmail.com>
2019-05-15 02:20:45 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,16 +1,39 @@
 def addition(num1, num2)
+  num3 = num1 + num2
+  
+  return num3
+  
+  
 end
 
 def subtraction(num1, num2)
+  
+  num3 = num1 - num2
+  
+  return num3
+  
 end
 
 def division(num1, num2)
+  
+  num3 = num1/num2
+  
+  return num3
+  
 end
 
 def multiplication(num1, num2)
+
+num3 = num1*num2
+
+return num3
+
 end
 
 def modulo(num1, num2)
+
+mod
+
 end
 
 def square_root(num)
